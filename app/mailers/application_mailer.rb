3e6ed5538e1718cc0ff0ca55dbf65dbc7cea0402
Michael Prins <michael@leap.ly>
2016-05-15 14:34:48 +0200

    Turn of sending mail for now

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Free Chapter Cape Town <mailgun@#{ENV['MAILGUN_DOMAIN'] || 'localhost'}>",
+  default from: 'Free Chapter Cape Town <fcctza@gmail.com>',
           reply_to: 'Michael Prins <michael.prins@me.com>'
   layout 'mailer'
 end
