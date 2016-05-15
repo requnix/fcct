620bbb9f9aa1636a4cd3311dbd3b71b98ccec015
Michael Prins <michael@leap.ly>
2016-05-15 14:16:48 +0200

    Use mailgun domain in mails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'Free Chapter Cape Town <fcctza@gmail.com>',
+  default from: "Free Chapter Cape Town <mailgun@#{ENV['MAILGUN_DOMAIN'] || 'localhost'}>",
           reply_to: 'Michael Prins <michael.prins@me.com>'
   layout 'mailer'
 end
